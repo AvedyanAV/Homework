095b49c9b060ad0364dbbd785f7ab2fcf8e1643d
Amajk <Amajk577@mail.ru>
2025-05-19 14:01:45 +0300

    init

---
+++ src/masks.py
@@ -0,0 +1,16 @@
+def get_mask_card_number(card_number: int) -> str:
+    """Функция возращает номер карты со скрытими символами"""
+    if len(str(card_number)) != 16:
+        return "Некорректный номер карты"
+
+    return f"{str(card_number)[:4]} {str(card_number)[4:6]}** **** {str(card_number)[-4:]}"
+
+
+def get_mask_account(account: int) -> str:
+    """Функция возращает номер счета со скрытими символами"""
+    if len(str(account)) != 20:
+        return "Некорректный номер счета"
+
+    return str(f"**{str(account)[-4:]}")
+
+
