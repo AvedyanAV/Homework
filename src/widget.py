a907bcd6a8e6387487db2760e1bd4191b32154a6
Amajk <Amajk577@mail.ru>
2025-05-19 16:23:33 +0300

    add def mask account card in file widget.py

--- src/widget.py
+++ src/widget.py
@@ -6,5 +6,22 @@ def get_date(date_str: str) -> str:
 
 
 def mask_account_card(card_number: str) -> str:
-    """"""
-    pass
+    """Функция возращает номер счета и карты со скрытими символами"""
+    if 'счет' in card_number.lower():
+        parts = card_number.split()
+
+        account_type = ' '.join(parts[:-1])
+        account_number = parts[-1]
+
+        from src.masks import get_mask_account
+        masked_number = get_mask_account(int(account_number))
+        return f"{account_type} {masked_number}"
+    else:
+        parts = card_number.split()
+
+        card_type = ' '.join(parts[:-1])
+        card_number = parts[-1]
+
+        from src.masks import get_mask_card_number
+        masked_number = get_mask_card_number(int(card_number))
+        return f"{card_type} {masked_number}"
