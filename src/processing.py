8d8baf5c852e365902fcf00f66c8e80a0e1dd2bb
Amajk <Amajk577@mail.ru>
2025-05-21 16:23:58 +0300

    fixes processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,7 +1,7 @@
-def filter_by_state(state_list: list, state="EXECUTED") -> list:
-    '''Функция принимает список словарей и опционально значение для ключа state
+def filter_by_state(state_list: list, state: str = "EXECUTED") -> list:
+    """Функция принимает список словарей и опционально значение для ключа state
     и возвращает новый список словарей, содержащий только те словари,
-    у которых ключ state соответствует указанному значению'''
+    у которых ключ state соответствует указанному значению"""
     new_state_list = list()
     for item in state_list:
         if item.get("state") == state:
@@ -9,7 +9,7 @@ def filter_by_state(state_list: list, state="EXECUTED") -> list:
     return new_state_list
 
 
-def sort_by_date(dict_list: list, reverse=True) -> list:
-    '''Функция принимает список словарей и сортирует их по дате.
-    По умолчанию сортировка на убывание'''
+def sort_by_date(dict_list: list, reverse: bool = True) -> list:
+    """Функция принимает список словарей и сортирует их по дате.
+    По умолчанию сортировка на убывание"""
     return sorted(dict_list, key=lambda x: x["date"], reverse=reverse)
