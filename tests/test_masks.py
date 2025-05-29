af33da84393d0fc614f613d6379fa643c1007734
Amajk <Amajk577@mail.ru>
2025-05-29 17:47:24 +0300

    fixes tests

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -2,51 +2,51 @@ import pytest
 from src.masks import get_mask_card_number, get_mask_account
 
 
-def test_get_mask_card_number_standard():
+def test_get_mask_card_number_standard() -> None:
     """Тест правильности маскирования номера карты"""
     assert get_mask_card_number(1234567890123456) == "1234 56** **** 3456"
 
 
 @pytest.fixture
-def len_numbers():
+def len_numbers() -> list:
     return [12345, 12345678901234567890, ""]
 
 
-def test_get_mask_card_number_len(len_numbers):
+def test_get_mask_card_number_len(len_numbers: str) -> None:
     """Тест номера карты с длиной меньше или больше 16 символов"""
     assert get_mask_card_number(len_numbers) == "Некорректный номер карты"
 
 
 @pytest.fixture
-def type_numbers():
+def type_numbers() -> list:
     return ["abcd567890123456", "1234 5678 9012 3456", "1234-5678-9012-3456"]
 
 
-def test_get_mask_card_number_type(type_numbers):
+def test_get_mask_card_number_type(type_numbers: str) -> None:
     """Тест работы функции на различных входных форматах номеров карт"""
     assert get_mask_card_number(type_numbers) == "Некорректный номер карты"
 
 
-def test_get_mask_account():
+def test_get_mask_account() -> None:
     """Тест правильности маскирования номера счета"""
     assert get_mask_account(12345678901234567892) == "**7892"
 
 
 @pytest.fixture
-def len_account():
+def len_account() -> list:
     return [12345, 12345678901234567891561650, ""]
 
 
-def test_get_mask_account_len(len_account):
+def test_get_mask_account_len(len_account: int) -> None:
     """Тест номера счета с длиной меньше или больше 20 символов"""
     assert get_mask_account(len_account) == "Некорректный номер счета"
 
 
 @pytest.fixture
-def type_account():
+def type_account() -> list:
     return ["abcd5678901234567895", "123456789012 34564589", "123456789012-34561523"]
 
 
-def test_get_mask_account_type(type_account):
+def test_get_mask_account_type(type_account: int) -> None:
     """Тест работы функции на различных входных форматах номеров счета"""
     assert get_mask_account(type_account) == "Некорректный номер счета"
--- tests/test_widget.py
+++ tests/test_widget.py
@@ -2,24 +2,24 @@ import pytest
 from src.widget import get_date, mask_account_card
 
 
-def test_get_date_standard():
+def test_get_date_standard() -> None:
     """Тестирование правильности преобразования даты"""
     assert get_date("2024-03-11T02:26:18.671407") == "11.03.2024"
 
 
 @pytest.fixture
-def date_type():
+def date_type() -> list:
     return ["12024-03-11T02:26:18.671407", "2024-50-11T02:26:18.671407", "2024-03-50T02:26:18.671407", ""]
 
 
-def test_get_date_type(date_type):
+def test_get_date_type(date_type: list) -> None:
     """Тест работы функции на некорректную дату"""
     for date in date_type:
         assert get_date(date) == "Некорректный формат даты"
 
 
 @pytest.fixture
-def date_day_in_month():
+def date_day_in_month() -> list:
     return [
         "2024-02-30T02:26:18.671407",
         "2024-04-31T02:26:18.671407",
@@ -29,7 +29,7 @@ def date_day_in_month():
     ]
 
 
-def test_date_day_in_month(date_day_in_month):
+def test_date_day_in_month(date_day_in_month: list) -> None:
     """Тест работы функции на количество дней в месяце"""
     for date in date_day_in_month:
         assert get_date(date) == "Некорректный формат даты"
@@ -42,7 +42,7 @@ def test_date_day_in_month(date_day_in_month):
         ("Счет 73654108430135874305", "Счет **4305"),
     ],
 )
-def test_mask_account_card_standard(account_card, expected):
+def test_mask_account_card_standard(account_card: str, expected: str) -> None:
     """Тест правильности маскирования номера карты, счета"""
     assert mask_account_card(account_card) == expected
 
@@ -55,6 +55,6 @@ def test_mask_account_card_standard(account_card, expected):
         ("MasterCard 7158300734726758", "MasterCard 7158 30** **** 6758"),
     ],
 )
-def test_mask_account_cards(account_card, expected):
+def test_mask_account_cards(account_card: str, expected: str) -> None:
     """Тесты с разными типами карт и счетов для проверки универсальности функции"""
     assert mask_account_card(account_card) == expected
