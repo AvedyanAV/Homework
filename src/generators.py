c3b81bbb094d91f7b1da367c7a83615a8447ce2e
Amajk <Amajk577@mail.ru>
2025-06-06 17:18:43 +0300

    add generator, test_generators

---
+++ src/generators.py
@@ -0,0 +1,20 @@
+#def filter_by_currency():
+#    pass
+
+
+#usd_transactions = filter_by_currency(transactions, "USD")
+#for _ in range(2):
+#    print(next(usd_transactions))
+
+
+#def transaction_descriptions():
+#    pass
+
+
+#descriptions = transaction_descriptions(transactions)
+#for _ in range(5):
+#    print(next(descriptions))
+
+
+def card_number_generator():
+    pass
---
+++ tests/test_generators.py
@@ -0,0 +1,13 @@
+import pytest
+
+
+def test_filter_by_currency():
+    pass
+
+
+def test_transaction_descriptions():
+    pass
+
+
+def test_card_number_generator():
+    pass
