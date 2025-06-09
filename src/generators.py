cba325fb85cd389cddbd88d7901feb3f98e11248
Amajk <Amajk577@mail.ru>
2025-06-09 14:31:36 +0300

    add generators.py, test_generators.py

--- src/generators.py
+++ src/generators.py
@@ -1,20 +1,21 @@
-#def filter_by_currency():
-#    pass
+def filter_by_currency(transactions: list, currency: str):
+    """Функция фильтрует транзакции по заданной валюте и возвращает итератор"""
+    for transaction in transactions:
+        operation_amount = transaction.get("operationAmount", {})
+        transaction_currency = operation_amount.get("currency", {}).get("code")
+        if transaction_currency == currency:
+            yield transaction
 
 
-#usd_transactions = filter_by_currency(transactions, "USD")
-#for _ in range(2):
-#    print(next(usd_transactions))
+def transaction_descriptions(transactions: list):
+    """Функция-Генератор, которая возвращает описания транзакций"""
+    for transaction in transactions:
+        yield transaction.get("description", "Описание отсутствует")
 
 
-#def transaction_descriptions():
-#    pass
-
-
-#descriptions = transaction_descriptions(transactions)
-#for _ in range(5):
-#    print(next(descriptions))
-
-
-def card_number_generator():
-    pass
+def card_number_generator(start: int, end: int):
+    """Генератор номеров банковских карт в заданном диапазоне"""
+    for number in range(start, end + 1):
+        card_str = f"{number:016d}"
+        formatted_card = " ".join([card_str[i:i + 4] for i in range(0, 16, 4)])
+        yield formatted_card
